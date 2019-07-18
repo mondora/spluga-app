c0326e492a9e3eef585d39540c1a2f2722eb92d4
Roberto Dell'Oro <roberto.delloro@mondora.com>
2019-07-18 15:25:33 +0200

    manage promise on inser

--- src/actions/companies.js
+++ src/actions/companies.js
@@ -5,17 +5,20 @@ export const GET_COMPANY_START = "GET_COMPANY_START";
 export const GET_COMPANY_SUCCESS = "GET_COMPANY_SUCCESS";
 export const GET_COMPANY_ERROR = "GET_COMPANY_ERROR";
 
+function getClient() {
+  // Get a client for your Stitch app, or instantiate a new one
+  return Stitch.hasAppClient(STITCH_APP_ID)
+    ? Stitch.getAppClient(STITCH_APP_ID)
+    : Stitch.initializeAppClient(STITCH_APP_ID);
+}
+
 export function getCompany(query) {
   return async dispatch => {
     dispatch({
       type: GET_COMPANY_START
     });
 
-    // Get a client for your Stitch app, or instantiate a new one
-    const client = Stitch.hasAppClient(STITCH_APP_ID)
-      ? Stitch.getAppClient(STITCH_APP_ID)
-      : Stitch.initializeAppClient(STITCH_APP_ID);
-
+    const client = getClient();
     const mongodb = client.getServiceClient(
       RemoteMongoClient.factory,
       "mongodb-atlas"
@@ -45,33 +48,31 @@ export const ADD_COMPANY_SUCCESS = "ADD_COMPANY_SUCCESS";
 export const ADD_COMPANY_ERROR = "ADD_COMPANY_ERROR";
 export const ADD_COMPANY_RESET = "ADD_COMPANY_RESET";
 
-export function addCompany(data, userId) {
+export function addCompany(data, ownerId) {
   return async dispatch => {
     dispatch({
       type: ADD_COMPANY_START
     });
 
-    try {
-      // Get a client for your Stitch app, or instantiate a new one
-      const client = Stitch.hasAppClient(STITCH_APP_ID)
-        ? Stitch.getAppClient(STITCH_APP_ID)
-        : Stitch.initializeAppClient(STITCH_APP_ID);
-
-      const mongodb = client.getServiceClient(
-        RemoteMongoClient.factory,
-        "mongodb-atlas"
-      );
-      const collection = mongodb.db(MONGO_DB_NAME).collection("companies");
-      collection.insertOne({ ownerId: userId, ...data });
-      dispatch({
-        type: ADD_COMPANY_SUCCESS
+    const client = getClient();
+    const mongodb = client.getServiceClient(
+      RemoteMongoClient.factory,
+      "mongodb-atlas"
+    );
+    const collection = mongodb.db(MONGO_DB_NAME).collection("companies");
+    collection
+      .insertOne({ ownerId, ...data })
+      .then(result => {
+        dispatch({
+          type: ADD_COMPANY_SUCCESS
+        });
+      })
+      .catch(e => {
+        dispatch({
+          type: ADD_COMPANY_ERROR,
+          error: e,
+          errorInfo: e
+        });
       });
-    } catch (e) {
-      dispatch({
-        type: ADD_COMPANY_ERROR,
-        error: e,
-        errorInfo: e
-      });
-    }
   };
 }
