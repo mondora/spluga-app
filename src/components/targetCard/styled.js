38915103b38ea07eb37a3fdbe92c13a9a3e57cbd
greta.lucchina <greta.lucchina@mondora.com>
2019-10-13 17:53:51 +0200

    feat: add TargetCard component

---
+++ src/components/targetCard/styled.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const CardContainer = styled.div`
+    height: 290px;
+    width: 360px;
+    box-sizing: border-box;
+    border-radius: 8px;
+    background-color: white;
+    border: 3px solid #bfbccb;
+    text-align: center;
+    margin: 20px;
+`;
+
+export const CardTitle = styled.h3`
+    margin-top: 20px;
+`;
+
+export const CardDescription = styled.div`
+    padding: 20px;
+`;
+
+export const CardProgress = styled.div`
+    padding: 10px;
+`;
