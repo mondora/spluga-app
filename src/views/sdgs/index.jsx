d9a076d545fc75bd85f3eed3f0047097cba6f123
greta.lucchina <greta.lucchina@mondora.com>
2020-06-22 16:53:17 +0200

    chore: update packages

--- src/views/sdgs/index.jsx
+++ src/views/sdgs/index.jsx
@@ -21,12 +21,20 @@ const SdgsWrapper = styled.div`
 `;
 
 const Images = styled.div`
+    width: 70%;
     display: flex;
+    flex-direction: column;
+`;
+
+const Text = styled.div`
+    width: 90%;
 `;
 
 export const Description = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
     padding: 24px;
-    align-self: flex-end;
 `;
 
 export const SdgsContainer = styled.div`
@@ -37,6 +45,8 @@ export const SdgsContainer = styled.div`
 `;
 
 export const Image = styled.div`
+    margin: 8px;
+    align-self: center;
     img {
         height: 100%;
     }
@@ -52,17 +62,16 @@ const Sdgs = () => {
             <div>{"Qual è il fine dell'SDG? Clicca sui box per conoscerli."}</div>
 
             <Container>
-                <div>
-                    <Description>
-                        <Images>
-                            <Image style={{ width: "70px", height: "70px" }}>
-                                <img src={SDGS_CIRCLE} alt="sdg-circle" />
-                            </Image>
-                            <Image style={{ width: "140px", height: "70px" }}>
-                                <img src={SDGS_LOGO} alt="sdg-circle" />
-                            </Image>
-                        </Images>
-                        <br />
+                <Description>
+                    <Images>
+                        <Image style={{ width: "70px", height: "70px" }}>
+                            <img src={SDGS_CIRCLE} alt="sdg-circle" />
+                        </Image>
+                        <Image style={{ width: "448px", height: "70px" }}>
+                            <img src={SDGS_LOGO} alt="sdg-circle" />
+                        </Image>
+                    </Images>
+                    <Text>
                         <div>{`Nel settembre 2015 più di 150 leader internazionali si sono incontrati alle Nazioni Unite per contribuire allo sviluppo globale, promuovere il benessere umano e proteggere l'ambiente.`}</div>
                         <div>{` La comunità degli Stati ha approvato l'Agenda 2030 per uno sviluppo sostenibile, i cui elementi essenziali sono i 17 obiettivi di sviluppo sostenibile (SDGs, Sustainable Development Goals) e i 169 sotto-obiettivi, i quali mirano a porre fine alla povertà, a lottare contro l'ineguaglianza e a crescere lo sviluppo sociale ed economico. Inoltre riprendono aspetti di fondamentale importanza per lo sviluppo sostenibile quali l'affrontare i cambiamenti climatici e costruire società pacifiche entro l'anno 2030. Gli SDG hanno validità universale, vale a dire che tutti i Paesi devono fornire un contributo per raggiungere gli obiettivi in base alle loro capacità. Spluga si basa sui Sustainable Development Goals per aiutare le aziende e le persone a misurare e migliorare il proprio impegno verso la società e l'ambiente, permettendo di valutare il contributo dato e contribuire a rendere il mondo un posto migliore.`}</div>
                         <div> {`Clicca sulle icone dei 17 Goals per saperne di più!`}</div>
@@ -74,8 +83,8 @@ const Sdgs = () => {
                         >
                             {"visita la pagina ufficiale per maggiori informazioni sugli SDGs"}
                         </a>
-                    </Description>
-                </div>
+                    </Text>
+                </Description>
                 <SdgsWrapper>
                     <SdgsContainer>
                         {sdgsUsed.map((sdg, index) => (
